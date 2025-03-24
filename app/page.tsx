0b960d9fc25f5bc7000829505d09f95dc7636063
Zoltan Fodor <zoltan@fdr.digital>
2025-03-24 13:54:30 +0100

    feat: add theme-provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col">
+    <div className="min-h-screen flex flex-col items-center justify-start relative">
+      <div className="absolute top-0 right-0 mt-10 mr-10">
+        <ThemeToggle />
+      </div>
+      <main className="my-32 mx-auto">
         <h1 className="text-4xl font-bold">
-          Toasted Studio - shadcn-ui components
+          <Link
+            href="https://toasted.studio"
+            className="hover:underline bg-[#efe8dd] text-black"
+          >
+            Toasted Studio
+          </Link>{" "}
+          - shadcn-ui components
         </h1>
       </main>
     </div>
